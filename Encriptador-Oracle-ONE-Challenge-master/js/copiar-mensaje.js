da94a285b9d4835b41ea057f47927bc684856203
Juan Pablo Reinoso <108629591+Jincoxx@users.noreply.github.com>
2023-09-29 20:11:35 -0500

    Delete Encriptador-Oracle-ONE-Challenge-master directory

--- Encriptador-Oracle-ONE-Challenge-master/js/copiar-mensaje.js
+++
@@ -1,9 +0,0 @@
-var botonCopiar = document.querySelector("#btn-copy");
-
-botonCopiar.addEventListener("click", function(event) {
-    event.preventDefault();
-    var copiarTexto = document.querySelector("#msg");
-    copiarTexto.select();
-    document.execCommand("copy");
-    alert("Texto copiado en el portapapeles.");
-});
